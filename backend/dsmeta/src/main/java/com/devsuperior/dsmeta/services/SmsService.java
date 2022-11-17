410b64958dde9da716d1439e3c46f9faf6b06de1
Vinicios Schons <viniciosschons@gmail.com>
2022-11-16 22:32:04 -0300

    Mensagem alterada

--- backend/dsmeta/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/dsmeta/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dsmeta.services;
 
+import java.text.DecimalFormat;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -34,7 +36,8 @@ public class SmsService {
 
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 		
-		String msg = "Vendedor " + sale.getSellerName() + " foi destaque em " + date + " com um total de R$ " + String.format("%.2f", sale.getAmount());
+		String msg = String.format("O vendedor %s foi destaque em %s com um total de R$ %.2f", sale.getSellerName(), date, sale.getAmount());
+
 		
 		Twilio.init(twilioSid, twilioKey);
 
