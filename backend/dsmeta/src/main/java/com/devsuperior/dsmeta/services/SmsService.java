966efac08bad2e8c982497126c3ae96797a6c88e
Vinicios Schons <viniciosschons@gmail.com>
2022-11-16 22:16:18 -0300

    Twilio SMS

--- backend/dsmeta/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/dsmeta/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -31,8 +31,10 @@ public class SmsService {
 	public void sendSms(Long saleId) {
 
 		Sale sale = saleRepository.findById(saleId).get();
+
+		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 		
-		String msg = "Vendedor " + sale.getSellerName();
+		String msg = "Vendedor " + sale.getSellerName() + " foi destaque em " + date + " com um total de R$ " + String.format("%.2f", sale.getAmount());
 		
 		Twilio.init(twilioSid, twilioKey);
 
